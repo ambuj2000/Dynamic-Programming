f6d3a5c1346106c61a271a0537213ea3394f5723
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2024-03-14 09:50:43 +0530

    Create Climbing Stairs.cpp

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,22 @@
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=3;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 1;//one step to reach 0th stair
+  dp[1]= 1;//same one step to reach 1st stair
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return 0;
+}
